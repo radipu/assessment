8089fcd6faa63d418f0b0208e5573e84af3e1911
Rohul Amin Mohammad Dipu <41869272+radipu@users.noreply.github.com>
2022-04-08 10:04:24 +0600

    assessment

---
+++ README.md
@@ -0,0 +1,2 @@
+# fn
+FariaNur.com website
---
+++ index.php
@@ -0,0 +1,234 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Faria Nur</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="shortcut icon" type="image/png" href="images/favicon.png">
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+		integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+		crossorigin="anonymous" referrerpolicy="no-referrer" />
+                
+	</head>
+	<body class="is-preload">
+
+		<nav class="navbar navbar-dark fixed-top">
+			<div class="container">
+
+		  <!-- Toggler/collapsibe Button -->
+		  <button class="navbar-toggler mx-auto d-block" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		  </button>
+		
+		  <!-- Navbar links -->
+		    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+			<ul class="navbar-nav ml-auto">
+			  <li class="nav-item">
+				<a class="nav-link" href="#story">Our Story</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="#women">Women Collection</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="#men">Men Collection</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="#contact">Contact</a>
+			  </li>
+			</ul>
+		  </div>
+			</div>
+		</nav>
+
+		<!-- Header -->
+		<!-- <nav class="navbar navbar-fixed navbar-expand-lg navbar-light">
+			<div class="container-fluid">
+			  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			  </button>
+			  <div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+				  <li class="nav-item">
+					<a class="nav-link active" aria-current="page" href="#story">Our Story</a>
+				  </li>
+				  <li class="nav-item">
+					<a class="nav-link" href="#women">Women Collection</a>
+				  </li>
+				  <li class="nav-item">
+					<a class="nav-link" href="#men">Men Collection</a>
+				  </li>
+				  <li class="nav-item">
+					<a class="nav-link" href="#contact">Contact</a>
+				  </li>
+				</ul>
+			  </div>
+			</div>
+		  </nav> -->
+
+		<!-- Intro -->
+			<section id="intro" class="main style1 dark fullscreen">
+				<div class="content">
+					<img src="images/FN_White.png" width="100" height="70" title="Potrait" alt="" />
+					<br>
+					<br>
+					<br>
+					<br>
+					<br>
+					<header>
+						<h2>Faria Nur</h2>
+					</header>
+					<br>
+					<br>
+					<br>
+					<br>
+					<br>
+					<p>
+						&copy;
+						<script>document.write(new Date().getFullYear())</script> Faria Nur, <a
+							href="https://www.farianur.com">farianur.com</a>.
+					</p>
+				</div>
+			</section>
+			<br>
+
+			<div class="container d-flex justify-content-center">
+					<p data-bs-toggle="modal" data-bs-target="#exampleModal_t" class="p-2">Terms</p>
+					
+		
+					<!-- Icons -->
+					<ul class="icons">
+						<li><a href="" target="_blank"><i class="fa-brands fa-instagram"></i><span class="label"></span></a>
+						</li>
+						<li><a href="" target="_blank"><i class="fa-brands fa-facebook"></i><span class="label"></span></a>
+						</li>
+						<li><a href="" target="_blank"><i class="fa-brands fa-youtube"></i><span class="label"></span></a>
+						</li>
+					</ul>
+		
+			</div>
+
+			<section id="contact">
+				<div class="modal fade" id="exampleModal_t" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalLabel">Terms</h5>
+								<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+							</div>
+							<div class="modal-body">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus magnam deleniti architecto
+								officiis, tempore culpa dolorem magni laborum nulla commodi quaerat aspernatur labore dolores
+								beatae libero dicta! Voluptates ullam alias error non quaerat unde amet consequuntur id culpa,
+								labore eveniet obcaecati molestias quis rerum atque vel minima neque dolores sed.
+								
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam minus ipsam mollitia esse totam! Quasi aliquam esse reiciendis asperiores rem?
+		
+							</div>
+							
+						</div>
+					</div>
+				</div>
+		
+			</section>
+
+		<!-- One -->
+		<section id="story" class="main style3 primary">
+			<div class="content">
+				<header>
+					<h2>Our Story</h2>
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo corporis magni reiciendis recusandae temporibus quae ullam, mollitia sapiente, nemo nulla molestias commodi eum deserunt magnam totam unde. In, cum tempora.</p>
+				</header>
+			</div>
+		</section>
+
+		<!-- Two -->
+		<section id="women" class="main style3 primary">
+			<div class="content">
+				<header>
+					<h2>Women Collection</h2>
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit!</p>
+				</header>
+
+				<!-- Gallery  -->
+					<div class="gallery">
+						<article class="from-left">
+							<a href="images/Dual_Bird_Midnight2.png" class="image fit"><img src="images/Dual_Bird_Midnight2.png" title="Dual bird at midnight" alt="" /></a>
+							<p>Photo Test 1</p>
+						</article>
+						<article class="from-right">
+							<a href="images/Dual_Bird_Midnight2.png" class="image fit"><img src="images/Dual_Bird_Midnight2.png" title="Dual bird at midnight" alt="" /></a>
+							<p>Photo Test 2</p>
+						</article>
+						<article class="from-left">
+							<a href="images/Dual_Bird_Midnight2.png" class="image fit"><img src="images/Dual_Bird_Midnight2.png" title="Dual bird at midnight" alt="" /></a>
+							<p>Photo Test 3</p>
+						</article>
+						<article class="from-right">
+							<a href="images/Dual_Bird_Midnight2.png" class="image fit"><img src="images/Dual_Bird_Midnight2.png" title="Dual bird at midnight" alt="" /></a>
+							<p>Photo Test 4</p>
+						</article>
+					</div>
+
+			</div>
+		</section>
+
+		<!-- Work -->
+			<section id="men" class="main style3 primary">
+				<div class="content">
+					<header>
+						<h2>Men Collection</h2>
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit!</p>
+					</header>
+
+					<!-- Gallery  -->
+					<div class="gallery">
+						<article class="from-left">
+							<a href="images/Dual_Bird_Midnight2.png" class="image fit"><img src="images/Dual_Bird_Midnight2.png" title="Dual bird at midnight" alt="" /></a>
+							<p>Photo Test 1</p>
+						</article>
+						<article class="from-right">
+							<a href="images/Dual_Bird_Midnight2.png" class="image fit"><img src="images/Dual_Bird_Midnight2.png" title="Dual bird at midnight" alt="" /></a>
+							<p>Photo Test 2</p>
+						</article>
+						<article class="from-left">
+							<a href="images/Dual_Bird_Midnight2.png" class="image fit"><img src="images/Dual_Bird_Midnight2.png" title="Dual bird at midnight" alt="" /></a>
+							<p>Photo Test 3</p>
+						</article>
+						<article class="from-right">
+							<a href="images/Dual_Bird_Midnight2.png" class="image fit"><img src="images/Dual_Bird_Midnight2.png" title="Dual bird at midnight" alt="" /></a>
+							<p>Photo Test 4</p>
+						</article>
+					</div>
+
+				</div>
+			</section>
+
+		<!-- Contact -->
+			<section id="contact" class="main style3 secondary">
+				<div class="content">
+					<header>
+						<h2>Contact Us</h2>
+					</header>
+					<p>Contact: +8801XXXXXXXXX</p>
+				</div>
+			</section>
+			<br>
+			<br>
+
+		<!-- Scripts -->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+			
+
+	</body>
+</html>
